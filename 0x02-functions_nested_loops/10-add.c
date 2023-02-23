c6ebc01444ee5efd3b58b686f45d330c317811d2
Anin Freddy <aninfreddy01@gmail.com>
2023-02-22 17:56:45 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - find the sum of two numbers
+ *
+ * Return: sum of two intergers
+ */
+int add(int, int);
+{
+	int sum;
+	int a, b;
+
+	sum = a + b;
+
+	return sum;
+}
+add(18,6);
