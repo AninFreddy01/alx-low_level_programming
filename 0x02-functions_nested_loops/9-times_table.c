0fb44dea105fb29e8df08e957f145432303aec64
Anin Freddy <aninfreddy01@gmail.com>
2023-02-22 17:16:06 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * times_table - Prints the mutiples of nine 9
+ *
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i * 9);
+	{
+}
